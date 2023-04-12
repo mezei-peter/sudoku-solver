0a9b191a3bcd18c6f5cc8b461aff5a160182c5d1
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-12 15:40:04 +0200

    Add file validation

--- src/logic/arguments_service.rs
+++ src/logic/arguments_service.rs
@@ -5,20 +5,41 @@ pub trait ArgsService {
     fn process(&self, args: &Vec<String>);
 }
 
-pub struct ArgsServiceImpl;
+pub struct ArgsServiceImpl {
+    defaul_grid_size: u8,
+}
 
 impl ArgsServiceImpl {
     fn handle_file_arg(&self, file_path: &String) {
-        println!("Input file path is: {}", file_path);
         let content =
             fs::read_to_string(file_path).expect(&format!("{} - Invalid file path", file_path));
-        println!("Contents: \n{}", content)
+        if self.invalidate_file(&file_path, &content) {
+            return;
+        }
+    }
+
+    fn invalidate_file(&self, file_path: &String, content: &String) -> bool {
+        if !file_path.ends_with(".sdm") {
+            println!("Invalid file format. Acceptable: .sdm");
+            return true;
+        }
+        if content.lines().count() == 1 {
+            if self.defaul_grid_size.pow(2) as usize != content.len() {
+                println!("Corrupted file data: length of lines are wrong.");
+                return true;
+            }
+        }
+        
+        println!("File validated.");
+        false
     }
 }
 
 impl ArgsService for ArgsServiceImpl {
     fn new() -> ArgsServiceImpl {
-        ArgsServiceImpl
+        ArgsServiceImpl {
+            defaul_grid_size: 9,
+        }
     }
 
     fn process(&self, args: &Vec<String>) {

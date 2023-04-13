41bc33e4d2ade53acc1cc7480e738910c9e8295a
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-13 13:25:04 +0200

    Initialize SudokuSolver

--- src/logic/puzzle_solver.rs
+++ src/logic/puzzle_solver.rs
@@ -1,5 +1,22 @@
 use crate::model::puzzle::Puzzle;
 
-pub trait SudokuSolver {
+pub trait PuzzleSolver {
     fn solve_puzzle(&self, puzzle: Puzzle) -> Puzzle;
+    fn solve_all_puzzles(&self, puzzles: Vec<Puzzle>) -> Vec<Puzzle>;
+}
+
+pub struct SudokuSolver;
+
+impl SudokuSolver {
+
+}
+
+impl PuzzleSolver for SudokuSolver {
+    fn solve_puzzle(&self, puzzle: Puzzle) -> Puzzle {
+        todo!()
+    }
+
+    fn solve_all_puzzles(&self, puzzles: Vec<Puzzle>) -> Vec<Puzzle> {
+        todo!()
+    }
 }

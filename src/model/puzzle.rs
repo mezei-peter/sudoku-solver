7d8e2eb367616397399ffe2c746d491fcfce8510
mezeipb <mezei.peter.barnabas@gmail.com>
2023-04-26 15:43:46 +0200

    Solve sudoku checking subgrids

--- src/model/puzzle.rs
+++ src/model/puzzle.rs
@@ -143,7 +143,38 @@ impl Puzzle {
     }
 
     fn none_in_subgrid(&self, position: (u8, u8), value: char) -> bool {
-        true
+        let mut subgrid: Vec<&Cell> = Vec::<&Cell>::new();
+        let (x, y) = position;
+        let subgrid_y_size: u8 = f32::sqrt(self.grid_size as f32).ceil() as u8;
+        let subgrid_x_size: u8 = f32::sqrt(self.grid_size as f32).floor() as u8;
+
+        let (bottom_y, top_y) = self.find_border_values(y, subgrid_y_size);
+        let (bottom_x, top_x) = self.find_border_values(x, subgrid_x_size);
+
+        for i in bottom_x..top_x + 1 {
+            for j in bottom_y..top_y + 1 {
+                subgrid.push(self.get_matrix_cell(i as usize, j as usize).unwrap());
+            }
+        }
+
+        let mut count: u8 = 0;
+        subgrid.iter().for_each(|cell| {
+            if cell.has_value(value) {
+                count += 1;
+            }
+        });
+        count == 0
+    }
+
+    fn find_border_values(&self, coordinate: u8, increment: u8) -> (u8, u8) {
+        let mut top_val: u8 = 0;
+
+        loop {
+            if coordinate < top_val {
+                return (top_val - increment, top_val - 1);
+            }
+            top_val += increment;
+        }
     }
 
     pub fn clone_matrix(&self) -> Vec<Vec<Cell>> {
